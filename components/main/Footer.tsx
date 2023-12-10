dd5413009e14a42b74ea93eab8bce4c6702966ad
krishnagitcontri <gitrepokrishna@gmail.com>
2023-12-10 21:18:50 +0530

    vercel fixes

--- components/main/Footer.tsx
+++ components/main/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <div className="w-full h-full bg-transparent text-gray-200 shadow-lg p-[15px] ">
       <div className="w-full flex flex-col items-center justify-center m-auto">
         <div className="mb-[20px] text-[15px] text-center">
-          &copy; Krishna's Portfolio 
+          &copy; Krishna&apos;s Portfolio 
           2023 Inc. All rights reserved
         </div>
       </div>
--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -25,7 +25,7 @@ const HeroContent = () => {
         >
           <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5" />
           <h1 className="Welcome-text text-[13px]">
-            Krishna's Portfolio
+            Krishna&apos;s Portfolio
           </h1>
         </motion.div>
 
